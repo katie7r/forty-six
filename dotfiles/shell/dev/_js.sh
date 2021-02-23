c3b39439fa582cef21cab1331fb8b4fd2ea01405
Katie Linero <katie.k7r@gmail.com>
2021-02-23 12:24:05 -0600

    More git aliases and double-loading NVM

--- dotfiles/shell/dev/_js.sh
+++ dotfiles/shell/dev/_js.sh
@@ -2,4 +2,5 @@
 
 # this loads nvm
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+  [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+  [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
